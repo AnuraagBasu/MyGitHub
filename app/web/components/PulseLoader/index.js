24bf696a1e3f21398e2013e8ca089c7c0a6b5841
Anuraag Basu <anuraag@quizizz.com>
2018-04-23 00:13:33 +0530

    PulseLoader component

---
+++ app/web/components/PulseLoader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Styles from './styles.scss'; // eslint-disable-line no-unused-vars
+
+const PulseLoader = () => {
+  return (
+    <div className="pulse-loader">
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default PulseLoader;
